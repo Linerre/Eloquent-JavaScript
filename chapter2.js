f8b26e81ad3c32febf34530bcb177e4742cc2bf6
Errelin Leon <49512984+Rust-Leon@users.noreply.github.com>
2019-10-01 15:30:44 +0800

    Update chapter2.js

--- chapter2.js
+++ chapter2.js
@@ -1,27 +1,27 @@
-// // question 1
-// let content = '';
-// for (let counter = 0; counter < 7; counter += 1) {
-//     console.log(content += '#')
-// }
+// question 1
+let content = '';
+for (let counter = 0; counter < 7; counter += 1) {
+    console.log(content += '#')
+}
 
-// let outPut = '';
-// for (let count = outPut.length; count < 7; count +=1) {
-//     outPut += '#';
-//     console.log(outPut)
-// }
+let outPut = '';
+for (let count = outPut.length; count < 7; count +=1) {
+    outPut += '#';
+    console.log(outPut)
+}
 
-// question 2
-// for (let n = 1; n < 101; n += 1) {
-//     if (n % 3 === 0 && n % 5 ===0) {
-//         console.log('FuzzBuzz');
-//     } else if (n % 3 === 0){
-//         console.log('Fuzz');
-//     } else if (n % 5 === 0) {
-//         console.log('Buzz')
-//     } else {
-//         console.log(n);
-//     }
-// }
+question 2
+for (let n = 1; n < 101; n += 1) {
+    if (n % 3 === 0 && n % 5 ===0) {
+        console.log('FuzzBuzz');
+    } else if (n % 3 === 0){
+        console.log('Fuzz');
+    } else if (n % 5 === 0) {
+        console.log('Buzz')
+    } else {
+        console.log(n);
+    }
+}
 
 // question 3
 let n = 1, m = 1, size = 8;
