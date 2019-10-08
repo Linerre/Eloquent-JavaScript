9b1b9c2f9baad3787ffb2ccf983caab5d692d228
Linerre <leon.errelin@outlook.com>
2019-10-08 22:44:16 +0800

    one way that not work

--- test.js
+++ test.js
@@ -13,12 +13,24 @@ class Group {
             this.array.push(element); // return array.push(element) would be wrong
             return this.array;
         } else if (this.array.includes(element)) {
-            return `The ${element} exists already in the group`;
+            return `The ${element} exists already in the group.`;
         } else {
             this.array.push(element);
             return this.array;
         }
-    }  
+    }
+    
+    delete(element) {
+        let index = this.array.indexOf(element);
+        // let span = this.array.length;
+        let firstHalf = this.array.slice(0, index),
+            secondHalf = this.array.slice(index+1,);
+        if (index === -1) {// element not in the array
+            return `Can't delete ${element} because the original array does not contain it.`
+        } else {// won't work since if element at the last postion, nothing will change
+            return firstHalf.concat(secondHalf);
+        }
+    }
 }
 
 
@@ -27,5 +39,14 @@ let group = new Group();
 group.add(2)
 group.add(5)
 group.add(4)
-console.log(group.add(2))
+console.log(group.delete(10))
+// Can't delete 10 because the original array does not contain it.
+group.add(10)
 console.log(group)
+// Group { array: [ 2, 5, 4, 10 ] }
+
+console.log(group.add(2))
+// The 2 exists already in the group.
+group.delete(10)
+console.log(group)
+// Group { array: [ 2, 5, 4 ] }
