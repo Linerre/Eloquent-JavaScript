6e9ca9f9fcda62f7ab895e1873a6d19b23597b8e
Linerre <leon.errelin@outlook.com>
2019-10-13 00:52:51 +0800

    add a demo file for more flexible testing

---
+++ demo.js
@@ -0,0 +1,8 @@
+// This file is used to test some small chank of code
+// as demo code which will be tranmited to test file later
+class GroupIterator {
+  constructor(Group) {
+    this.Group = Group
+  }
+
+}
