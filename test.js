2012682b90de629fa9d0c325b5f0e9fd129f1fec
Linerre <leon.errelin@outlook.com>
2019-10-14 00:50:28 +0800

    for the last question in chapter6

--- test.js
+++ test.js
@@ -5,95 +5,22 @@
 // For code that do work, please refer to chapter_i file, 
 // where i points to the corresponding chapter number
 
-// qustion2
-let testArray1 = [1, 2, 3, 4],
-    testArray2 = [20, 'NYU', 'Jessie'];
+// Borrowing a Method
+// The solution can be found and has been well explained by
+// MDN doc at here: 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty
 
-class Group {
-    constructor() {
-        this.array = [];
-    }
-    
-    add(element) {
-            //if the array is empty
-        if (this.array.length === 0) {
-            this.array.push(element); // return array.push(element) would be wrong
-            return this.array;
-        } else if (this.array.includes(element)) {
-            return `The ${element} exists already in the group.`;
-        } else {
-            this.array.push(element);
-            return this.array;
-        }
-    }
-    
-    delete(element) {
-        let index = this.array.indexOf(element);
-        let span = this.array.length;
-        let firstHalf = this.array.slice(0, index),
-            secondHalf = this.array.slice(index+1,);
-        if (index === -1) {// element not in the array
-            return `Can't delete ${element} because the original array does not contain it.`
-        } else if (index === span - 1) {//element at the last postion
-            this.array.pop(element);
-            return this.array;
-        } else if (index === 0) {//element at the beginning
-            this.array.shift(element);
-            return this.array;
-        } else {// element in the middle
-            return firstHalf.concat(secondHalf);
-        }
-    }
 
-    has(element) {
-        if(this.array.includes(element)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+// This is why I often fear I misunderstand the author.
+// When she said 'borrowing a method', I was thinking of inheritance so far as to create 
+// a new one myself. But the solution is quite simple and self-explainatory
 
-    static from(anArray) {
-        let insider = new Group();
-        for (let element of anArray) {
-            insider.add(element);
-        }
-        return insider;
-    }
 
-    [Symbol.iterator]() {
-        let index = 0;
-        let grouper = this.array;
-        return {
-            next: () => {
-                if (index < grouper.length) {
-                    let element = grouper[index];
-                    index += 1;
-                    return { value: element, done: false }
-                } else {
-                    return { done: true }
-                }
-            }    
-        }   
-    }
-}
+let map = {one: true, two: true, hasOwnProperty: true};
 
-for (let value of Group.from(["a", "b", "c"])) {
-    console.log(value);
-  }
-  // → a
-  // → b
-  // → c
 
-// // test groupTwo
-// let groupTwo = Group.from(testArray2)
-// console.log(groupTwo.has('NYU'))
-// //true
-// console.log(groupTwo)
-// //Group { array: [ 20, 'NYU', 'Jessie' ] }
-// console.log(groupTwo.delete('10'))
-// // Can't delete 10 because the original array does not contain it.
-// groupTwo.delete(20)
-// //
-// console.log(groupTwo)
-// // [ 'NYU', 'Jessie' ]
+// Fix this call
+console.log(({}).hasOwnProperty.call(map ,"one")); //method one
+console.log(Object.prototype.hasOwnProperty.call(map ,"one"));  //method two
+
+// → true
