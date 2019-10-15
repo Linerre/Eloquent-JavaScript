c177892f25478b8479d218f9fba1f23051c9a8ea
Linerre <leon.errelin@outlook.com>
2019-10-15 21:53:15 +0800

    using demo code to test and review some ideas or methods

--- demo.js
+++ demo.js
@@ -5,3 +5,17 @@
 // Most of the time this file is EMPTY
 // Sometimes there are a few lines of code here
 // But the code might be ugly and deadly wrong
+const name_gender = [
+    'Leon-male',
+    'Vickie-female',
+    'Ashley-female',
+    'Edward-male',
+    'Jessie-female',
+    'Cathie-female'
+] 
+
+for (let [name, gender] of name_gender.map(person => person.split('-'))) {
+    console.log([name, gender])
+}
+
+console.log(name_gender.map(person => person.split('-')))
