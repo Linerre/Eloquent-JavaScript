a7afd09f763549d9cf39d17298447fa927493f55
Linerre <leon.errelin@outlook.com>
2019-10-16 10:17:08 +0800

    understood the first delivery robot project

--- project_a_robot.js
+++ project_a_robot.js
@@ -29,21 +29,34 @@ const roads = [// the village roadpath
   const roadGraph = buildGraph(roads);
   // console.log(roadGraph)
 
-  class VillgeState{
-    constructor(place, parcels) {
+  class VillageState {
+    constructor(place, parcels) {//parcel_example = { place: swh, address: destination }
       this.place = place;
       this.parcels = parcels; 
     }
 
     move(destination) {
-      if(!roadGraph[this.place].includes(destination)) {
-        return this;
+      if(!roadGraph[this.place].includes(destination)) {//check if from-to path is valid
+        return this; //if not, return the old state
       } else {
-        let parcels = this.parcels.map(p => {
-          if(p.place != this.place) return p;
-          return {place: destination, address: p.address};
-        }).filter(p => {p.place != p.address});
-        return new VillgeState(destination, parcels);
+        let parcels = this.parcels.map(p => {//create a new state for moving
+          if (p.place != this.place) return p;//move!
+          return {place: destination, address: p.address}; //out of if-loop, still need return sth
+        }).filter(p => {p.place != p.address});//get rid of those delivered
+        return new VillageState(destination, parcels); //keep delivering
       } 
     }
   }
+
+  let first = new VillageState(
+    "Post Office",
+    [{place: "Post Office", address: "Alice's House"}]
+  );
+  let next = first.move("Alice's House");
+  
+  console.log(next.place);
+  // → Alice's House
+  console.log(next.parcels);
+  // → []
+  console.log(first.place);
+  // → Post Office
