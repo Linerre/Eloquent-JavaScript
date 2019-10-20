061a2d30b625cabe9413e8d0878d1f67964bdb50
Linerre <leon.errelin@outlook.com>
2019-10-20 23:21:43 +0800

    to be done later

--- chapter7.js
+++ chapter7.js
@@ -1,1 +1,16 @@
 // chapter 7 Robot project
+// Persistent group
+class PGroup {
+    // Your code here
+  }
+  
+  let a = PGroup.empty.add("a");
+  let ab = a.add("b");
+  let b = ab.delete("a");
+  
+  console.log(b.has("b"));
+  // → true
+  console.log(a.has("b"));
+  // → false
+  console.log(b.has("a"));
+  // → false
