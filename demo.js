cd76849c759f642c9b56ec0460c9b2af302a2e07
Linerre <leon.errelin@outlook.com>
2019-10-25 00:53:01 +0800

    chapter8 ex done!

--- demo.js
+++ demo.js
@@ -5,19 +5,3 @@
 // Most of the time this file is EMPTY
 // Sometimes there are a few lines of code here
 // But the code might be ugly and deadly wrong
-
-
-const box = {
-    locked: true,
-    unlock() { this.locked = false; },
-    lock() { this.locked = true;  },
-    _content: [],
-    get content() {
-      if (this.locked) throw new Error("Locked!");
-      return this._content;
-    }
-  };
-
-console.log(box.locked);
-console.log(box.unlock()); //unlocked
-console.log(box.locked); // it is unlocked and therefore not true -- false
