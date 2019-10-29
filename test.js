d86ca110abb6953756d53463198160868c5d5f69
Linerre <leon.errelin@outlook.com>
2019-10-29 23:30:55 +0800

    the wrong opposite way of replacing

--- test.js
+++ test.js
@@ -5,3 +5,14 @@
 // For code that do work, please refer to chapter_i file, 
 // where i points to the corresponding chapter number
 
+//Quoting style
+let text = "'I'm the cook,' he said, 'it's my job.'";
+// Change this call.
+console.log(text.replace(/'\w+'\w+\s[\w\s,.!?]+'/g, "$&"));
+// → "I'm the cook," he said, "it's my job." 
+
+// this way will not work since it is quite the opposite of the right one
+// think about it! 
+// when the match is 'I'm the cook,', how can you use a string as a whole
+// to replace the original one? the target should be the very single quotes!
+// so you are likely to think in the other way around.
