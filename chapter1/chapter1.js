9f7eb118f7d4a359b0b2e33228b832c2179aefff
Linerre <leon.errelin@outlook.com>
2019-10-29 15:31:00 +0800

    chapter1 direcotry added

---
+++ chapter1/chapter1.js
@@ -0,0 +1,10 @@
+//nothing here since this chapter asks no exercises to be done
+//
+//yet it's fine to keep all chapters directories consistent by
+//adding sth here
+//
+//the best two things I leaned from this chapter are:
+//first, this operation:
+console.log((3>1) ? 'Yes' : 'No'
+//second, automatic type converion
+console.log('5' - 1)
