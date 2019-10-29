869e32022117475ac9cfd7da96eaacc4b3f54712
Linerre <leon.errelin@outlook.com>
2019-10-29 14:39:33 +0800

    global option confusion

--- test.js
+++ test.js
@@ -15,9 +15,58 @@
 // When she said 'borrowing a method', I was thinking of inheritance so far as to create 
 // a new one myself. But the solution is quite simple and self-explainatory
 
-let reg = /ca[rt]\w?/;
+// this will work
+let reg1 = /ca[rt]\w?/; 
 const tests = ['my car', 'bad cats', 'his cat', 'your cars'];
+
 for (let str of tests) {
-    console.log(reg.test(str));
+    console.log(reg1.test(str));
     // console.log(str.match(reg));
 }
+
+// true
+// true
+// true
+// true
+
+// this will work too!
+let reg2 = /ca[rt]\w?/g; // note the 'g' option here
+for (let str of tests) {
+    console.log(reg2.test(str));
+    console.log(str.match(reg2));
+}
+// true
+// true
+// true
+// true
+// true
+// [ 'car' ]
+// true
+// [ 'cats' ]
+// true
+// [ 'cat' ]
+// true
+// [ 'cars' ]
+
+// HOWEVER, this will NOT work:
+let reg3 = /ca[rt]\w?/g; // note the 'g' option here
+for (let str of tests) {
+    console.log(reg3.test(str));
+    // console.log(str.match(reg2));
+}
+// true
+// true
+// true
+// true
+// true
+// [ 'car' ]
+// true
+// [ 'cats' ]
+// true
+// [ 'cat' ]
+// true
+// [ 'cars' ]
+// true
+// false   <---- why?
+// true
+// false   <---- why?
