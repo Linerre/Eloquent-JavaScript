847eddb627ea29c227eec5c5008bc2b9be637ddb
Linerre <leon.errelin@outlook.com>
2019-10-29 14:47:28 +0800

    modified test.js

--- test.js
+++ test.js
@@ -5,15 +5,6 @@
 // For code that do work, please refer to chapter_i file, 
 // where i points to the corresponding chapter number
 
-// Borrowing a Method
-// The solution can be found and has been well explained by
-// MDN doc at here: 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty
-
-
-// This is why I often fear I misunderstand the author.
-// When she said 'borrowing a method', I was thinking of inheritance so far as to create 
-// a new one myself. But the solution is quite simple and self-explainatory
 
 // this will work
 let reg1 = /ca[rt]\w?/; 
