b68bff04cdbeb18d7d5d7190f916d148752a33d5
Linerre <leon.errelin@outlook.com>
2019-11-01 01:19:08 +0800

    finished the 2nd exercise

--- chapter9/chp9.js
+++ chapter9/chp9.js
@@ -38,3 +38,25 @@ function verify(regexp, yes, no) {
         console.log(`Unexpected match for '${str}'`);
     }
 }
+
+// Quoting style
+//Quoting style
+let text = "'I'm the cook,' he said, 'it's my job.' 'You're right!'";
+// Change this call.
+// console.log(text.replace(/'\w+'\w+\s[\w\s,.!?]+'/g, "$&"));
+// → "I'm the cook," he said, "it's my job." 
+
+// this way will not work since it is quite the opposite of the right one
+// think about it! 
+// when the match is 'I'm the cook,', how can you use a string as a whole
+// to replace the original one? the target should be the very single quotes!
+// so you are likely to think in the other way around.
+
+//however, using a function as the 2nd arg of .replace method
+//could solve the problem, though it is NOT smart
+// Method One
+console.log(text.replace(/'\w+'\w+\s[\w\s,.!?]+'/g, str => {return '"' + str.slice(1, -1) + '"'; }))
+// "I'm the cook," he said, "it's my job."
+
+// method Two
+console.log(text.replace(/'([\w]+\W)([^']+\W)'/g, "\"$1$2\""))
