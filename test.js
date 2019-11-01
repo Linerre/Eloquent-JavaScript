db3ec528564f739e29aefe83ed5b3087e890cf71
Linerre <leon.errelin@outlook.com>
2019-11-01 23:41:10 +0800

    get ready for Q3

--- test.js
+++ test.js
@@ -5,22 +5,21 @@
 // For code that do work, please refer to chapter_i file, 
 // where i points to the corresponding chapter number
 
-//Quoting style
-let text = "'I'm the cook,' he said, 'it's my job.' 'You're right!'";
-// Change this call.
-// console.log(text.replace(/'\w+'\w+\s[\w\s,.!?]+'/g, "$&"));
-// → "I'm the cook," he said, "it's my job." 
+// Numbers again
+// Fill in this regular expression.
+let number = /^...$/;
 
-// this way will not work since it is quite the opposite of the right one
-// think about it! 
-// when the match is 'I'm the cook,', how can you use a string as a whole
-// to replace the original one? the target should be the very single quotes!
-// so you are likely to think in the other way around.
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12"]) {
+    if (!number.test(str)) {
+        console.log(`Failed to match '${str}'`);
+    }
+}
 
-//however, using a function as the 2nd arg of .replace method
-//could solve the problem, though it is NOT smart
-console.log(text.replace(/'\w+'\w+\s[\w\s,.!?]+'/g, str => {return '"' + str.slice(1, -1) + '"'; }))
-// "I'm the cook," he said, "it's my job."
-
-// method two
-console.log(text.replace(/'([\w]+\W)([^']+\W)'/g, "\"$1$2\""))
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+                 ".5.", "1f5", "."]) {
+    if (number.test(str)) {
+        console.log(`Incorrectly accepted '${str}'`);
+    }
+}
