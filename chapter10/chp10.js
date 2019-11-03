b4f3d6ade300c0dd365a327ed939b89dd9edf857
Linerre <leon.errelin@outlook.com>
2019-11-03 16:26:41 +0800

    get ready for ex in chp10

---
+++ chapter10/chp10.js
@@ -0,0 +1,30 @@
+// A modular robot
+// I'll skip this exercise for the time being (and probably forever), 
+// because it is too big for me who is in a bit hurry to finish the book 
+// and is expecting short, focused exercises instaed.
+// Maybe in the future when I need to work out a module/package structure of my own,
+// I'll review this drill and try to finish it. 
+
+// Yet I strongly recommend readding the author's hints, which, again, is very
+// thoughtful and inspiring. 
+
+// Roads module
+// Add dependencies and exports
+
+const roads = [
+    "Alice's House-Bob's House",   "Alice's House-Cabin",
+    "Alice's House-Post Office",   "Bob's House-Town Hall",
+    "Daria's House-Ernie's House", "Daria's House-Town Hall",
+    "Ernie's House-Grete's House", "Grete's House-Farm",
+    "Grete's House-Shop",          "Marketplace-Farm",
+    "Marketplace-Post Office",     "Marketplace-Shop",
+    "Marketplace-Town Hall",       "Shop-Town Hall"
+  ];
+
+
+
+
+
+  // Circular dependencies
+  // This ex will be ignored too, because as far as I understand, it looks 
+  // more like for fun. Anyway, the hints inspire and explain well. Read it.
